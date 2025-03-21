d32ba15cffff70493ff244a82bdec94df158ea2c
Evander Stewart <evanderstewart@gmail.com>
2025-03-21 01:04:28 +0000

    venv changes

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\evand\OneDrive\Documents\QA\Degree\Lvl 5\Software Engineering & Agile\Retake\Assignment\Software Engineering & Agile - Web Application\my_flask_app\venv
+set VIRTUAL_ENV=C:\Users\evand\OneDrive\Documents\QA\Degree\Lvl 5\Software Engineering & Agile\Retake\Assignment\Software Engineering & Agile - Web Application\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
